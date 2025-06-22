4ee38080712567a5b549f1c869a9d423f018788e
conuong <hothaipt123@gmail.com>
2025-06-22 16:20:05 +0700

    xong header transion va sua slider

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,32 +1,32 @@
-const sliderItem = document.querySelectorAll('.slider-item');
-for (let index = 0;index <sliderItem.length;index++){
+    const sliderItem = document.querySelectorAll('.slider-item');
+    for (let index = 0;index <sliderItem.length;index++){
 
-    sliderItem[index].style.left = index *100 + "%";
+        sliderItem[index].style.left = index *100 + "%";
 
-}
-
-const sliderItems = document.querySelector('.slider-items');
-const arrowRight = document.querySelector('.fa-arrow-right');
-const arrowLeft = document.querySelector('.fa-arrow-left');
-let i =0 
-arrowRight.addEventListener('click',()=> {
-    i++
-    if(i < sliderItem.length){
-        sliderItems.style.left = -i * 100 + "%";
-    } else{
-        return false
     }
-});
 
-arrowLeft.addEventListener('click',()=> {
-     if(i <= 0){
-        return false
-    } else{
-        i--
-        sliderItems.style.left = -i * 100 + "%";
-        console.log(i);
-    }
-});
+    const sliderItems = document.querySelector('.slider-items');
+    const arrowRight = document.querySelector('.fa-arrow-right');
+    const arrowLeft = document.querySelector('.fa-arrow-left');
+    let i =0 
+    arrowRight.addEventListener('click',()=> {
+        i++
+        if(i < sliderItem.length){
+            sliderItems.style.left = -i * 100 + "%";
+        } else{
+            i=0
+            sliderItems.style.left = -i * 100 + "%";
+        }
+    });
+
+    arrowLeft.addEventListener('click',()=> {
+        if(i <= 0){
+            return false
+        } else{
+            i--
+            sliderItems.style.left = -i * 100 + "%";
+        }
+    });
 
 // function autoSlider() {
 //     if(i < sliderItem.length-1  ){
@@ -49,3 +49,12 @@ menuBar.addEventListener('click',()=>{
 })
 
 
+//sticky header
+window.addEventListener('scroll',()=>{
+    if(scrollY > 50){
+        document.querySelector('#header').classList.add('active');
+    }
+    else{
+        document.querySelector('#header').classList.remove('active');
+    }
+});
