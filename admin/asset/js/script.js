1483803ff23bc110ca33198423aa462a4b61d88a
conuong <hothaipt123@gmail.com>
2025-07-03 00:11:26 +0700

    done header

--- admin/asset/js/script.js
+++ admin/asset/js/script.js
@@ -1,16 +1,29 @@
-const menuLi = document.querySelectorAll('.admin-sidebar-content > ul > li >a')
-const subMenu = document.querySelectorAll('.sub-menu')
+const menuLi = document.querySelectorAll('.admin-sidebar-content > ul > li > a');
+const subMenus = document.querySelectorAll('.sub-menu');
 
- for (let index = 0; index < menuLi.length; index++) {
-    menuLi[index].addEventListener('click',(e) =>{
-        e.preventDefault()
+for (let i = 0; i < menuLi.length; i++) {
+    menuLi[i].addEventListener('click', (e) => {
+        e.preventDefault();
 
-        for (let index = 0; index < subMenu.length; index++) {
-            subMenu[index].setAttribute("style","height:0px")
-            
+        // Đóng tất cả submenu
+        for (let j = 0; j < subMenus.length; j++) {
+            subMenus[j].setAttribute("style", "height: 0px");
         }
 
-        const submenuHeight =menuLi[index].parentNode.querySelector('ul .sub-menu-items').offsetHeight
-        menuLi[index].parentNode.querySelector('ul').setAttribute("style" , "height:"+submenuHeight+"px")
-    })
- }
+        // Mở submenu tương ứng nếu có
+        const currentSubMenu = menuLi[i].parentNode.querySelector('.sub-menu');
+        if (currentSubMenu) {
+            const submenuHeight = currentSubMenu.querySelector('.sub-menu-items')?.offsetHeight || 0;
+            currentSubMenu.setAttribute("style", "height:" + submenuHeight + "px");
+        }
+    });
+}
+
+// document.addEventListener('click', function (e) {
+//     const sidebar = document.querySelector('.admin-sidebar-content');
+//     if (!sidebar.contains(e.target)) {
+//         for (let j = 0; j < subMenus.length; j++) {
+//             subMenus[j].setAttribute("style", "height: 0px");
+//         }
+//     }
+// });
